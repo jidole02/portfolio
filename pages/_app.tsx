32cf2d9921cec37e964bed1fc14dc3a3340db18e
jiwon <jidole041214@naver.com>
2022-02-03 16:09:21 +0900

    create init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
-export default MyApp
+export default MyApp;
