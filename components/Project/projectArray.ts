e84d137393ebde117fcc75be6c2f1218555fed0b
jiwon <jidole041214@naver.com>
2022-03-03 16:51:50 +0900

    update project

--- components/Project/projectArray.ts
+++ components/Project/projectArray.ts
@@ -35,11 +35,4 @@ export const ProjectArray: props[] = [
     tag: ["풀스택 개발", "개인프로젝트", "책 집필 및 출판 서비스"],
     projectName: "sellery",
   },
-  {
-    stack: ["Next JS", "Context API", "Redux", "Typescript"],
-    description: "학교 동아리 지원, 이제는 온라인에서 해봐!",
-    title: "대동여지도",
-    tag: ["20만건 이상의 요청", "개발 인원 11명", "웹 소켓 채팅", "동아리 SNS"],
-    projectName: "ddyzd",
-  },
 ];
