4300ace537babf5c00f4d7dff451f3fccdb00a9d
jiwon <jidole041214@naver.com>
2022-03-07 19:07:03 +0900

    fix png->PNG

--- components/Project/projectArray.ts
+++ components/Project/projectArray.ts
@@ -13,7 +13,7 @@ export const ProjectArray: props[] = [
     description: "운영체제 스케줄링 계산? 자동으로 되니까",
     title: "스케줄링 GUI",
     tag: ["개인프로젝트", "운영체제 수업 실사용", "스케줄링 계산 자동화"],
-    projectName: "operating",
+    projectName: "op",
   },
   {
     stack: ["Next JS", "Context API", "XD"],
@@ -26,13 +26,13 @@ export const ProjectArray: props[] = [
       "개발 인원 4명",
       "인디음악 플랫폼",
     ],
-    projectName: "kunder",
+    projectName: "kundermusic",
   },
   {
     stack: ["Next JS", "Typescript", "express", "node JS", "mongo DB", "XD"],
     description: "나만의 책을 출판해보는거 어때?",
     title: "샐러리북",
     tag: ["풀스택 개발", "개인프로젝트", "책 집필 및 출판 서비스"],
-    projectName: "sellery",
+    projectName: "sellerybook",
   },
 ];
